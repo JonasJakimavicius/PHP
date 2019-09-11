7028c420f586a725c685ffd41971f0c7ce3f2ad9
Jonas <jakimaviciusjonas@gmail.com>
2019-09-11 12:11:52 +0300

    Rand  RGB spalvu keitimas

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <html>
 <head>
-    <meta charset="UTF-8">
-    <title>PHP lydės ir <?php print date('Y.m.d', strtotime('+' . rand(1, 10) . 'year')); ?></title>
+    <meta charset="UTF-8"/>
+    <title>PHP lydės ir <?php print date('Y.m.d', strtotime('+' . rand(1, 10000000) . 'minutes')); ?></title>
 </head>
-
-<body>
-<h1>Jonas - Galbūt turėsiu <?php print  rand(1, 5); ?> </h1>
-<p> Trump'as nebebus prezidentu: <?php print date('Y.m.d', strtotime('+ ' . rand(2, 10) . 'year')); ?> </p>
+<body style = 'background-color: rgb(<?php print rand(1, 255) . ',' . rand(1, 255) . ',' . rand(1, 255); ?>)'>
+    <h1 style = 'font-size: <?php print rand(1, 30); ?>'>Aš keičiu savo dydį</h1>
+    <p style='color:rgb(<?php print rand(1, 255) . ', ' . rand(1, 255) . ', ' . rand(1, 255); ?>)'>  Aš keičiu savo spalvą </p>
 </body>
 </html>
 
