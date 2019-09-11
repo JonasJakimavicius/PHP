b336bad26d7a99152dd823b8d0a47e20dc3b81f0
Jonas <jakimaviciusjonas@gmail.com>
2019-09-11 12:43:52 +0300

    Bomba

--- index.php
+++ index.php
@@ -3,10 +3,8 @@
     <meta charset="UTF-8"/>
     <title>PHP lydės ir <?php print date('Y.m.d', strtotime('+' . rand(1, 10000000) . 'minutes')); ?></title>
 </head>
-<body style='background-color: rgb(<?php print rand(1, 255) . ',' . rand(1, 255) . ',' . rand(1, 255); ?>)'>
-    <h1 style='font-size: <?php print rand(1, 30); ?>'>Aš keičiu savo dydį</h1>
-    <p style='color:rgb(<?php print rand(1, 255) . ', ' . rand(1, 255) . ', ' . rand(1, 255); ?>)'>
-        Aš keičiu savo spalvą </p>
+<body>
+    <img src="https://media.istockphoto.com/vectors/cartoon-bomb-illustration-vector-id842671590?k=6&m=842671590&s=612x612&w=0&h=D1A2a--svXcKUyosW2-StYy-2cUNz7c_Zf3RlcqJDK8="
+         style="height: <?php print date('s'); ?>">
 </body>
 </html>
-
