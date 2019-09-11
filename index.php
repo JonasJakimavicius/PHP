e426d2f7517422756c8bb21d9fbb82731fa31b9a
Jonas <jakimaviciusjonas@gmail.com>
2019-09-11 10:52:21 +0300

    Strtotime +rand funckija

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8">
     <title>PHP lydės ir <?php print date('Y.m.d', strtotime('+' . rand(1, 10) . 'year')); ?></title>
 </head>
+
 <body>
 <h1>Jonas - Galbūt turėsiu <?php print  rand(1, 5); ?> </h1>
 <p> Trump'as nebebus prezidentu: <?php print date('Y.m.d', strtotime('+ ' . rand(2, 10) . 'year')); ?> </p>
