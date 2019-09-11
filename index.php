0231874af90d3e33e5dd0bc2b61ea6935b1e09d0
Jonas <jakimaviciusjonas@gmail.com>
2019-09-11 12:45:05 +0300

    Bomba su laiku

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
 <body>
     <img src="https://media.istockphoto.com/vectors/cartoon-bomb-illustration-vector-id842671590?k=6&m=842671590&s=612x612&w=0&h=D1A2a--svXcKUyosW2-StYy-2cUNz7c_Zf3RlcqJDK8="
          style="height: <?php print date('s'); ?>">
+    <p> <?php print date('s'); ?> </p>
 </body>
 </html>
