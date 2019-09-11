a86b7d3ddebd78b3c9a6619f1d8e56105059a678
Jonas <jakimaviciusjonas@gmail.com>
2019-09-11 12:12:53 +0300

    Rand  RGB spalvu keitimas

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
     <meta charset="UTF-8"/>
     <title>PHP lydės ir <?php print date('Y.m.d', strtotime('+' . rand(1, 10000000) . 'minutes')); ?></title>
 </head>
-<body style = 'background-color: rgb(<?php print rand(1, 255) . ',' . rand(1, 255) . ',' . rand(1, 255); ?>)'>
-    <h1 style = 'font-size: <?php print rand(1, 30); ?>'>Aš keičiu savo dydį</h1>
-    <p style='color:rgb(<?php print rand(1, 255) . ', ' . rand(1, 255) . ', ' . rand(1, 255); ?>)'>  Aš keičiu savo spalvą </p>
+<body style='background-color: rgb(<?php print rand(1, 255) . ',' . rand(1, 255) . ',' . rand(1, 255); ?>)'>
+    <h1 style='font-size: <?php print rand(1, 30); ?>'>Aš keičiu savo dydį</h1>
+    <p style='color:rgb(<?php print rand(1, 255) . ', ' . rand(1, 255) . ', ' . rand(1, 255); ?>)'>
+        Aš keičiu savo spalvą </p>
 </body>
 </html>
 
