3b346069d5100db8bceaf7e725aa391c4c05272a
Jonas <jakimaviciusjonas@gmail.com>
2019-09-12 10:10:12 +0300

    img variable

--- index.php
+++ index.php
@@ -1,22 +1,17 @@
 <?php
 
-$string = 'Stringas';
-$int = 8;
-$bool = true;
-$null =null;
+$img = 'https://image.shutterstock.com/image-photo/beautiful-water-drop-on-dandelion-260nw-789676552.jpg';
+
 
 ?>
-
 <hmtl>
     <head>
         <meta charset="UTF-8">
     </head>
     <body>
-        <ul>
-            <li><?php print $string; ?></li>
-            <li><?php print $int; ?></li>
-            <li><?php print $bool; ?></li>
-            <li><?php print $null; ?></li>
-        </ul>
+        <img src="<?php print $img; ?>">
+        <img src="<?php print $img; ?>">
+        <img src="<?php print $img; ?>">
+        <img src="<?php print $img; ?>">
     </body>
 </hmtl>
