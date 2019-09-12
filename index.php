90dc476cfeb3e1e05554c51e5dcdf3c6f226c0cc
Jonas <jakimaviciusjonas@gmail.com>
2019-09-12 10:42:08 +0300

    Kodo taisymas pakoreguota

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-$sudas1 = rand(1, 100);
-$sudas2 = rand(101, 200);
-$sudas3 = rand(201, 300);
+$graza1 = rand(1, 100);
+$graza2 = rand(101, 200);
+$skola = rand(201, 300);
 
 ?>
 <hmtl>
@@ -12,9 +12,9 @@ $sudas3 = rand(201, 300);
     <body>
         <div>
             <h1>Skolos skaičiuoklė</h1>
-            <h3>Jei paėmei <?php print $sudas1; ?> eurų</h3>
-            <h3>su dviem kabančiais grąžinsi <?php print $sudas3; ?></h3>
-            <h3>su vienu kabančiu grąžinsi <?php print $sudas2; ?></h3>
+            <h3>Jei paėmei <?php print $skola; ?> eurų</h3>
+            <h3> su dviem kabančiais grąžinsi <?php print $graza2; ?> </h3>
+            <h3> su vienu kabančiu grąžinsi <?php print $graza1; ?>.</h3>
         </div>
     </body>
 </hmtl>
