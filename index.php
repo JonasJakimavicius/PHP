eb85e46e694479fc08f90979020570f1da5f0758
Jonas <jakimaviciusjonas@gmail.com>
2019-09-12 10:37:05 +0300

    Kodo taisymas

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-$img = 'https://image.shutterstock.com/image-photo/beautiful-water-drop-on-dandelion-260nw-789676552.jpg';
+$sudas1 = rand(1, 100);
+$sudas2 = rand(101, 200);
+$sudas3 = rand(201, 300);
 
 ?>
 <hmtl>
@@ -8,9 +10,11 @@ $img = 'https://image.shutterstock.com/image-photo/beautiful-water-drop-on-dande
         <meta charset="UTF-8">
     </head>
     <body>
-        <img src="<?php print $img; ?>">
-        <img src="<?php print $img; ?>">
-        <img src="<?php print $img; ?>">
-        <img src="<?php print $img; ?>">
+        <div>
+            <h1>Skolos skaičiuoklė</h1>
+            <h3>Jei paėmei <?php print $sudas1; ?> eurų</h3>
+            <h3>su dviem kabančiais grąžinsi <?php print $sudas3; ?></h3>
+            <h3>su vienu kabančiu grąžinsi <?php print $sudas2; ?></h3>
+        </div>
     </body>
 </hmtl>
