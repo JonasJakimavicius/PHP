6f547d5263bf895f08070deb0d0cb91013bad0ca
Jonas <jakimaviciusjonas@gmail.com>
2019-09-12 12:10:08 +0300

    Laikas isnesti siuksles pataisytas trecia kart.

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 $siusklines_turis = 40;
 $siuskliu_turis_per_d = 15;
 $max_kaupo_turis = rand(1, 20);
-$likesLaikas = ($siusklines_turis + $max_kaupo_turis) / $siuskliu_turis_per_d;
-$diena = date('Y.m.d', strtotime(+floor($likesLaikas) . 'day'));
+$dienos = ($siusklines_turis + $max_kaupo_turis) / $siuskliu_turis_per_d;
+$data = date('Y.m.d', strtotime(+floor($dienos) . 'day'));
 
 ?>
 <hmtl>
@@ -12,7 +12,7 @@ $diena = date('Y.m.d', strtotime(+floor($likesLaikas) . 'day'));
         <meta charset="UTF-8">
     </head>
     <body>
-        <p>Po <?php print floor($likesLaikas) . '(' . $diena . ')'; ?> pirk gėlių ir šampano, jeigu nori išvengti
+        <p>Po <?php print floor($dienos) . '(' . $data . ')'; ?> pirk gėlių ir šampano, jeigu nori išvengti
             konflikto.
         </p>
     </body>
