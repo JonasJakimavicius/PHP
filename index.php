33b18b4d084a0e9c929232778f914b798adb302e
Jonas <jakimaviciusjonas@gmail.com>
2019-09-13 12:06:31 +0300

    buitine skaiciuokle-sofa

--- index.php
+++ index.php
@@ -5,15 +5,20 @@ $grizai_isgeres = rand(0, 1);
 
 if ($grizai_velai && !$grizai_isgeres) {
     $pasekmes = 'grįžai vėlai';
+    $sofa = 'nemiegosi';
 } elseif ($grizai_velai && !$grizai_isgeres) {
     $pasekmes = 'grįžai vėlai ir išgėręs';
+    $sofa = 'Miegosi';
 } elseif (!$grizai_velai && $grizai_isgeres) {
     $pasekmes = 'grįžai išgėręs';
+    $sofa = 'nemiegosi';
 } else {
     $pasekmes = 'nieko nepadarei';
+    $sofa = 'nemiegosi';
 }
 
 $aplinkybes = 'Situacija ' . $pasekmes;
+$miegosi = 'Išvada: ' . $sofa . ' ant sofos'
 
 ?>
 <html>
@@ -23,5 +28,6 @@ $aplinkybes = 'Situacija ' . $pasekmes;
 <body>
     <h1>Buitinė skaičiuoklė</h1>
     <h2><?php print $aplinkybes; ?></h2>
+    <h3> <?php print $miegosi; ?></h3>
 </body>
 </html>
