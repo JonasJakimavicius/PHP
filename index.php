ce14deef0fccfa9e0fb8af27913dc01ff76f7f22
Jonas <jakimaviciusjonas@gmail.com>
2019-09-13 09:49:03 +0300

    Kuro sanaudu skaiciuokle (pataisyta2)

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 $distance = rand(1, 100);
 $consumption = 7.5;
 $price_1 = 1.3;
-$fuel_total = $distance * $consumption/100;
+$fuel_total = $distance * $consumption / 100;
 $price_trip = $fuel_total * $price_1;
 $nuvaziuotas_kelias = 'Nuvažiuota distancija: ' . $distance . ' km.';
 $kuro_sanaudos = 'Sunaudota ' . $fuel_total . ' l. kuro.';
-$islaidos = 'Kaina ' . $price_trip . ' pinigų';
+$islaidos = 'Kaina ' . $price_trip . ' pinigų.';
 ?>
 <hmtl>
     <head>
@@ -15,8 +15,10 @@ $islaidos = 'Kaina ' . $price_trip . ' pinigų';
     </head>
     <body>
         <h1>Kelionės skaičiuoklė</h1>
-        <li><?php print $nuvaziuotas_kelias; ?></li>
-        <li><?php print $kuro_sanaudos; ?></li>
-        <li><?php print $islaidos; ?></li>
+        <ul>
+            <li><?php print $nuvaziuotas_kelias; ?></li>
+            <li><?php print $kuro_sanaudos; ?></li>
+            <li><?php print $islaidos; ?></li>
+        </ul>
     </body>
 </hmtl>
