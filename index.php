c3ecaa3b1efd9310ce40751b625f65ad5d8332dc
Jonas <jakimaviciusjonas@gmail.com>
2019-09-13 11:38:25 +0300

    buitine skaiciuokle(clean)

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 $grizai_velai = rand(0, 1);
 $grizai_isgeres = rand(0, 1);
 
-if ($grizai_velai == true && !$grizai_isgeres == true) {
+if ($grizai_velai && !$grizai_isgeres) {
     $pasekmes = 'grįžai vėlai';
-} elseif ($grizai_velai == true && $grizai_isgeres == true) {
+} elseif ($grizai_velai && !$grizai_isgeres) {
     $pasekmes = 'grįžai vėlai ir išgėręs';
-} elseif (!$grizai_velai == true && $grizai_isgeres == true) {
+} elseif (!$grizai_velai && $grizai_isgeres) {
     $pasekmes = 'grįžai išgėręs';
 } else {
     $pasekmes = 'nieko nepadarei';
-};
+}
 
 $aplinkybes = 'Situacija ' . $pasekmes;
 
