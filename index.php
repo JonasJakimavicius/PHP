2a88051160a8db4fe14487fbdf56ac268982cce3
Jonas <jakimaviciusjonas@gmail.com>
2019-09-16 10:53:06 +0300

    For ciklas - kates/sunys

--- index.php
+++ index.php
@@ -1,9 +1,35 @@
 <?php
 
-$x = rand(1, 10);
+$kates = rand(1, 3);
+$sunys = rand(1, 3);
+$pavyko = rand(0, 1);
+$katasuniai = 0;
 
-for ($i = 0; $x > $i; $i++) {
-    print 'Tai yra ' . $i . ' -tasis ciklas. ';
+for ($i = 1; $i <= $kates; $i++) {
+
+    for ($x = 1; $x <= $sunys; $x++) {
+
+        if ($katasuniai = $kates) {
+            break;
+        }
+
+        if ($pavyko) {
+            $katasuniai++;
+        }
+    }
 }
 
+$dalyviai = "Dalyvavo $kates katės ir $sunys šunys.";
+$katasuniu_iseiga = "Katašunių išeiga: $katasuniai.";
+
 ?>
+<html>
+<head>
+    <meta charet="UTF-8">
+</head>
+<body>
+    <h1>Katašunių išeiga</h1>
+    <h2><?php print $dalyviai; ?></h2>
+    <h3><?php print $katasuniu_iseiga; ?></h3>
+</body>
+</html>
