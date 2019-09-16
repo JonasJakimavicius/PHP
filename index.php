3626469af895c97f67ce69acc70a34cc30bd104c
Jonas <jakimaviciusjonas@gmail.com>
2019-09-16 13:17:21 +0300

    for ciklas cigaretem

--- index.php
+++ index.php
@@ -1,19 +1,31 @@
 <?php
 
-$months = 12;
-$wallet = 1000;
-$month_income = 700;
+$days = 365;
+$pack_price = 3.5;
+$count_ttl = 0;
 
-$h2_text = "Po $months m., prognuozuojamas likutis: $wallet.";
+for ($i = 1; $i < $days; $i++) {
+    $current_day = date('l');
 
-for ($month = 1; $month <= 12; $month++) {
-    $month_expenses = rand(1, 1000);
-    $wallet += $month_income - $month_expenses;
-    if ($wallet < 0) {
-        $h2_text = "Atsargiai, $month mėnesį gali baigtis pinigai.";
-        break;
+    if ($current_day == 'Monday' || $current_day == 'Tuesday' || $current_day == 'Wednesday' || $current_day == 'Thursday' || $current_day == 'Friday') {
+        $cigs_mon_fri = rand(3, 4);
+        $count_ttl += $cigs_mon_fri;
+        $current_day = date('l', strtotime("+ $i days"));
+
+    } elseif ($current_day == 'Saturday') {
+        $cigs_sat = rand(10, 20);
+        $count_ttl += $cigs_sat;
+        $current_day = date('l', strtotime("+ $i days"));
     }
+
+    $cigs_sun = rand(1, 5);
+    $count_ttl += $cigs_sun;
+    $current_day = date('l', strtotime("+ $i days"));
 }
+
+$price_ttl = $count_ttl / 20 * $pack_price;
+
+$h2_text = "Per $days dienas, surūkysiu $count_ttl cigarečių už $price_ttl eur.";
 
 ?>
 <html>
@@ -21,7 +33,7 @@ for ($month = 1; $month <= 12; $month++) {
     <meta charset="UTF-8">
 </head>
 <body>
-    <h1>Wallet forecast</h1>
+    <h1>Mano dūmų skaičiuoklė</h1>
     <h2><?php print $h2_text; ?></h2>
 </body>
 </html>
