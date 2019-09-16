5076b21f0600cd7c60de963265cbc48b12eae954
Jonas <jakimaviciusjonas@gmail.com>
2019-09-16 14:59:26 +0300

    for ciklas cigaretem (prarukytas laikas)

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 $days = 365;
 $pack_price = 3.5;
 $count_ttl = 0;
+$time_per_cig = 5;
 
 for ($i = 0; $i < $days; $i++) {
     $current_day = date('N', strtotime("+ $i days"));
@@ -14,15 +15,17 @@ for ($i = 0; $i < $days; $i++) {
     } elseif ($current_day == 6) {
         $cigs_sat = rand(10, 20);
         $count_ttl += $cigs_sat;
+
+    } else {
+        $cigs_sun = rand(1, 5);
+        $count_ttl += $cigs_sun;
     }
-    $cigs_sun = rand(1, 5);
-    $count_ttl += $cigs_sun;
 }
-
-
 $price_ttl = $count_ttl / 20 * $pack_price;
+$time_ttl = $count_ttl * $time_per_cig;
 
 $h2_text = "Per $days dienas, surūkysiu $count_ttl cigarečių už $price_ttl eur.";
+$h3_text = "Viso traukdamas prastovėsiu $time_ttl valandų"
 
 ?>
 <html>
@@ -32,5 +35,6 @@ $h2_text = "Per $days dienas, surūkysiu $count_ttl cigarečių už $price_ttl e
 <body>
     <h1>Mano dūmų skaičiuoklė</h1>
     <h2><?php print $h2_text; ?></h2>
+    <h3><?php print $h3_text; ?></h3>
 </body>
 </html>
