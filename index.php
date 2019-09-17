45947d50cd5271d88109986dd4a5d2d0dfe1963b
Jonas <jakimaviciusjonas@gmail.com>
2019-09-17 13:05:31 +0300

    Masinos galesiu nusipirkti

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 
-$months = 24;
-$car_price_new = 3000;
+$months = 0;
+$car_price_new = 30000;
 $depreciation = 2;
-$car_price_used = $car_price_new;
+$santaupos = 15000;
 
-for ($i = 0; $i < $months; $i++) {
+for ($car_price_used = $car_price_new, $months = 0; $santaupos < $car_price_used; $months++, $car_price_used) {
     $car_price_used -= ($car_price_used / 100 * $depreciation);
 }
 
-$depr_perc = 100 - ($car_price_used / $car_price_new * 100);
+$depr_perc = round(100 - ($car_price_used / $car_price_new * 100));
 
 $h2_text = "Naujos mašinos kaina: $car_price_new";
-$h3_text = "Po $months mėn., mašinos vertė bus: $car_price_used eur";
+$h3_text = "Mašiną galėsi nusipirkti po $months mėn., kai jos vertė bus: $car_price_used eur.";
 $h4_text = "Mašina nuvertės $depr_perc proc";
 
 ?>
