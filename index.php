d68d9cecd012629898cec1a7e2cd05382249a535
Jonas <jakimaviciusjonas@gmail.com>
2019-09-17 09:05:52 +0300

    for ciklas cigaretem atspausdintos cigaretes su pakeliais(hack)

--- index.php
+++ index.php
@@ -26,21 +26,32 @@ for ($i = 0; $i < $days; $i++) {
 }
 $price_ttl = $count_ttl / 20 * $pack_price;
 $time_ttl = $count_ttl * $time_per_cig;
+$cig_left = $count_ttl;
 
 $h2_text = "Per $days dienas, surūkysiu $count_ttl cigarečių už $price_ttl eur.";
-$h3_text = "Viso traukdamas prastovėsiu $time_ttl valandų"
+$h3_text = "Viso traukdamas prastovėsiu $time_ttl valandų";
 
 ?>
 <html>
 <head>
     <meta charset="UTF-8">
     <style>
-        .img {
+        .cig_img {
             height: 10px;
             width: 10px;
             background-position: center;
             background-image: url("http://www.rw-designer.com/icon-image/11364-256x256x32.png");
             background-size: cover;
+            display: inline-block;
+            float:left;
+        }
+
+        .package {
+            height: 20px;
+            width: 100px;
+            display: inline-block;
+            border: solid 1px;
+            float:left;
         }
     </style>
 </head>
@@ -48,10 +59,17 @@ $h3_text = "Viso traukdamas prastovėsiu $time_ttl valandų"
     <h1>Mano dūmų skaičiuoklė</h1>
     <h2><?php print $h2_text; ?></h2>
     <h3><?php print $h3_text; ?></h3>
-    <?php for ($i = 0;
-               $i < $count_ttl;
-               $i++): ?>
-        <div class="<?php print'img'; ?>"></div>
-    <?php endfor; ?>
+
+    <?php for ($cig = 0; $cig <= $count_ttl;$cig += 20): ?>
+    <?php $likutis = $cig_left > 20 ? 20 : $cig_left; ?>
+
+    <div class="<?php print'package'; ?>">
+        <?php for ($i = 0; $i < $likutis; $i++): ?>
+            <?php $cig_left--; ?>
+
+            <div class="<?php print'cig_img'; ?>"></div>
+        <?php endfor; ?>
+        <?php endfor; ?>
+    </div>
 </body>
 </html>
