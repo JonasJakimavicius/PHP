2c3cdc46de42ba9b5de0e7965121efa7b41ec5bf
Jonas <jakimaviciusjonas@gmail.com>
2019-09-17 11:51:04 +0300

    Masinos verte

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ for ($i = 0; $i < $months; $i++) {
     $car_price_used -= ($car_price_new / 100 * $depreciation);
 }
 
-$depr_perc = 100-($car_price_used / $car_price_new * 100);
-$h2_text = "Naujos mašinos kaina: $$car_price_new";
+$depr_perc = 100 - ($car_price_used / $car_price_new * 100);
+$h2_text = "Naujos mašinos kaina: $car_price_new";
 $h3_text = "Po $months mėn., mašinos vertė bus: $car_price_used eur";
 $h4_text = "Mašina nuvertės $depr_perc proc";
 
