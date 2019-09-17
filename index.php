a600e97e769c93813e1f176ed516f0f968c36696
Jonas <jakimaviciusjonas@gmail.com>
2019-09-17 10:14:49 +0300

    for grikiai

--- index.php
+++ index.php
@@ -1,67 +1,33 @@
 <?php
 
-$days = 365;
-$pack_price = 3.5;
-$count_ttl = 0;
-$time_per_cig = 5;
+$grikiai = 5000;
+$grikiu_likutis = $grikiai;
+$days = 0;
+$data = date('Y-m-d', strtotime("+ $days days"));
 
-for ($i = 0; $i < $days; $i++) {
-    $current_day = date('N', strtotime("+ $i days"));
+for ($i = 0; $grikiu_likutis > 0; $i + $per_day) {
+    $per_day = rand(200, 500);
 
-    if ($current_day <= 5) {
-        $cigs_mon_fri = rand(3, 4);
-        $count_ttl += $cigs_mon_fri;
-
-    } elseif ($current_day == 6) {
-        $cigs_sat = rand(10, 20);
-        $count_ttl += $cigs_sat;
+    if ($per_day < $grikiu_likutis) {
+        $grikiu_likutis -= $per_day;
+        $days++;
 
     } else {
-        $cigs_sun = rand(1, 5);
-        $count_ttl += $cigs_sun;
+        break;
     }
 }
 
-$price_ttl = $count_ttl / 20 * $pack_price;
-$time_ttl = $count_ttl * $time_per_cig;
-$cig_left = $count_ttl;
-
-$h2_text = "Per $days dienas, surūkysiu $count_ttl cigarečių už $price_ttl eur.";
-$h3_text = "Viso traukdamas prastovėsiu $time_ttl valandų";
+$h2_text = "Rasta grikių: $grikiai g.";
+$h3_text = "Išgyvensiu dar $days dienas, iki $data."
 
 ?>
 <html>
 <head>
     <meta charset="UTF-8">
-    <style>
-        .cig_img {
-            height: 10px;
-            width: 10px;
-            background-image: url("http://www.rw-designer.com/icon-image/11364-256x256x32.png");
-            background-size: cover;
-            display: inline-block;
-            float: left;
-        }
-
-        .package {
-            width: 100px;
-            height: 20px;
-            display: inline-block;
-            border: solid 1px;
-            margin: 2px 3px;
-        }
-    </style>
 </head>
 <body>
-    <h1>Mano dūmų skaičiuoklė</h1>
+    <h1>Kiek dienų galėsi valgyti grikius?</h1>
     <h2><?php print $h2_text; ?></h2>
     <h3><?php print $h3_text; ?></h3>
-    <?php for ($cig = 0; $cig <= $count_ttl; $cig += 20): ?>
-        <div class="<?php print'package'; ?>">
-            <?php for ($i = ($cig_left > 20 ? 20 : $cig_left); $i > 0; $i--, $cig_left--): ?>
-                <div class="<?php print'cig_img'; ?>"></div>
-            <?php endfor; ?>
-        </div>
-    <?php endfor; ?>
 </body>
 </html>
