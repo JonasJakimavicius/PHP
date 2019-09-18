7a9743db81f2fc15f7cc752139a17b5eb3c2142f
Jonas <jakimaviciusjonas@gmail.com>
2019-09-18 11:55:14 +0300

    mano atmintis ciklas/masyvas

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ $mano_atmintis = ['Penktadienis', 'Paskaita', 'Baras', 'Alus', 'Draugai'];
     <h1>Kas buvo penktadienį?</h1>
     <h2>Jono atmintis</h2>
     <ul>
-        <?php foreach ($mano_atmintis as $value): ?>
-            <li><?php print $value; ?></li>
+        <?php foreach ($mano_atmintis as $prisiminimas): ?>
+            <li><?php print $prisiminimas; ?></li>
         <?php endforeach; ?>
     </ul>
 </body>
