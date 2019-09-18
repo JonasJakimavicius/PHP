f51a8f71cde196d4715b57490fdee977db9938fd
Jonas <jakimaviciusjonas@gmail.com>
2019-09-18 11:35:24 +0300

    Masyvai basics (lentynos) clean

--- index.php
+++ index.php
@@ -1,52 +1,53 @@
 <?php
 
-$lentynos = [
-    'pirma_lentyna' =>
-        [
+$lentynos =
+    [
+        'pirma_lentyna' =>
             [
-                'name' => 'Kiaušiniai',
-                'img' => 'url',
-                'quantity' => 3,
+                [
+                    'name' => 'Kiaušiniai',
+                    'img' => 'url',
+                    'quantity' => 3,
+                ],
+                [
+                    'name' => 'Grietinė',
+                    'img' => 'url',
+                    'quantity' => 1,
+                ],
+                [
+                    'name' => 'Jogurtas',
+                    'img' => 'url',
+                    'quantity' => 2,
+                ],
+                [
+                    'name' => 'Žuvis',
+                    'img' => 'url',
+                    'quantity' => 0,
+                ],
             ],
+        'antra_lentyna' =>
             [
-                'name' => 'Grietinė',
-                'img' => 'url',
-                'quantity' => 1,
+                [
+                    'name' => 'Pienas',
+                    'img' => 'url',
+                    'quantity' => 1,
+                ],
+                [
+                    'name' => 'Kefyras',
+                    'img' => '',
+                    'quantity' => 0,
+                ],
+                [
+                    'name' => 'Degtinė',
+                    'img' => 'url',
+                    'quantity' => 2,
+                ],
+                [
+                    'name' => 'Kečupas',
+                    'img' => 'url',
+                    'quantity' => 2,
+                ],
             ],
-            [
-                'name' => 'Jogurtas',
-                'img' => 'url',
-                'quantity' => 2,
-            ],
-            [
-                'name' => 'Žuvis',
-                'img' => '',
-                'quantity' => 0,
-            ],
-        ],
-    'antra_lentyna' =>
-        [
-            [
-                'name' => 'Pienas',
-                'img' => 'url',
-                'quantity' => 1,
-            ],
-            [
-                'name' => 'Kefyras',
-                'img' => '',
-                'quantity' => 0,
-            ],
-            [
-                'name' => 'Degtinė',
-                'img' => 'url',
-                'quantity' => 2,
-            ],
-            [
-                'name' => 'Kečupas',
-                'img' => '',
-                'quantity' => 2,
-            ],
-        ],
-];
+    ];
 
 var_dump($lentynos);
