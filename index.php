13009c601d354b0ced804a54ff9c1e0b66b805b4
Jonas <jakimaviciusjonas@gmail.com>
2019-09-18 14:56:17 +0300

    Namu darbas- mano ir draugo atmintis ir bendra uzduotis

--- index.php
+++ index.php
@@ -2,11 +2,22 @@
 
 $mano_atmintis = ['Penktadienis', 'Paskaita', 'Baras', 'Alus', 'Draugai'];
 $draugo_atmintis = ['Penktadienis', 'Paskaita', 'Baras', 'Alus', 'Blackout'];
+$bendra_atmintis = [];
 
 $count_mano_atmintis = count($mano_atmintis) - 1;
 $rand_flashback = rand(0, $count_mano_atmintis);
 
 $flashback_text = "Flashback $rand_flashback : $mano_atmintis[$rand_flashback]";
+
+foreach ($mano_atmintis as $mano_prisiminimas) {
+
+    foreach ($draugo_atmintis as $draugo_prisiminimas) {
+
+        if ($mano_prisiminimas == $draugo_prisiminimas) {
+            $bendra_atmintis[] = $mano_prisiminimas;
+        }
+    }
+}
 
 ?>
 <html>
@@ -25,6 +36,12 @@ $flashback_text = "Flashback $rand_flashback : $mano_atmintis[$rand_flashback]";
     <h2>Draugo atmintis</h2>
     <ul>
         <?php foreach ($draugo_atmintis as $prisiminimas): ?>
+            <li><?php print $prisiminimas; ?></li>
+        <?php endforeach; ?>
+    </ul>
+    <h3>Sutapę prisiminimai:</h3>
+    <ul>
+        <?php foreach ($bendra_atmintis as $prisiminimas): ?>
             <li><?php print $prisiminimas; ?></li>
         <?php endforeach; ?>
     </ul>
