c2863af2e5883a44b6ad3750b3b6cba4cae6feee
Jonas <jakimaviciusjonas@gmail.com>
2019-09-19 15:56:43 +0300

    Namu darbai, 5 uzduotis random masyvo generavimas (ka moteris turi rankinuke) ir tikimybės ištraukti tokį daiktą paskaičiavimas (tokių pat daiktų rankinuke gali būti daugiau nei vienas).

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <?php
 
-$name = ['lupdazis', 'kremas', 'bananas', 'grafke', 'veidrodelis'];
-$size = [10, 20, 30, 40, 60, 80, 99, 90, 500];
+$name = ['lupdazis', 'kremas', 'bananas', 'mandarinas', 'veidrodis'];
+$size = [10, 20, 30, 40, 60, 40, 30, 99];
 $rankinukas = [];
 
+$random_number = rand(1, 50);
 $name_array_count = (count($name) - 1);
 $size_array_count = (count($size) - 1);
-$random_number = rand(0, 50);
 
 for ($i = 0; $i < $random_number; $i++) {
     $rankinukas[$i]['name'] = $name[rand(0, $name_array_count)];
     $rankinukas[$i]['size'] = $size[rand(0, $size_array_count)];
+}
+
+$rankinukas_array_count = (count($rankinukas));
+
+foreach ($rankinukas as $daiktas_id => $daiktas) {
+    $rankinukas[$daiktas_id]['repeat'] = 0;
+    foreach ($rankinukas as $copy_id => $copy) {
+        if ($daiktas['name'] === $copy['name'] && $daiktas['size'] === $copy['size']) {
+            $rankinukas[$daiktas_id]['repeat']++;
+            $rankinukas[$daiktas_id]['probability'] = round($rankinukas[$daiktas_id]['repeat'] / $rankinukas_array_count * 100, 2);
+        }
+    }
 }
 
 ?>
@@ -22,7 +34,7 @@ for ($i = 0; $i < $random_number; $i++) {
     <h1>Ką moteris turi rankinuke</h1>
     <ul>
         <?php foreach ($rankinukas as $daiktas): ?>
-            <li>  <?php print "{$daiktas['name']} užima {$daiktas['size']} cm3"; ?></li>
+            <li>  <?php print "{$daiktas['name']} užima {$daiktas['size']} cm3. Tikimybė rasti: {$daiktas['probability']} %"; ?></li>
         <?php endforeach; ?>
     </ul>
 </body>
