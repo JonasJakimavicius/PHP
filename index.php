ddd0b0ee107c0f1bd3cdefb0a524e585e25775c1
Jonas <jakimaviciusjonas@gmail.com>
2019-09-19 14:39:48 +0300

    Namu darbai, 2 uzduotis

--- index.php
+++ index.php
@@ -1,69 +1,27 @@
 <?php
 
-$bank_report =
-    [
-        [
-            'name' => 'Iki darbo uzmokescio',
-            'amount' => 600,
-        ],
-        [
-            'name' => 'Kalvariju nacnykas',
-            'amount' => -15,
-        ],
-        [
-            'name' => 'Meat lovers',
-            'amount' => -20,
-        ],
-        [
-            'name' => 'Gringo',
-            'amount' => -40,
+$zodziai = ['Petras', 'Laukė', 'Prie', 'Maximos', 'Antradienį'];
+$rasinys = [];
 
-        ],
-    ];
+$zodziai_count = (count($zodziai) - 1);
 
-$balance = 0;
-$total_income = 0;
-$total_expense = 0;
-
-foreach ($bank_report as $money_transfer_id => $money_transfer) {
-    $balance += $money_transfer['amount'];
-
-    if ($bank_report[$money_transfer_id]['amount'] > 0) {
-        $bank_report[$money_transfer_id]['css_class'] = 'income';
-        $total_income += $money_transfer['amount'];
-    } else {
-        $bank_report[$money_transfer_id]['css_class'] = 'expense';
-        $total_expense += $money_transfer['amount'];
-    }
-    $bank_report[$money_transfer_id]['text'] = "{$money_transfer['name']} : {$money_transfer['amount']}";
+for ($i = 0;
+     $i < rand(0, 50);
+     $i++) {
+    $rasinys[] = $zodziai[rand(0, $zodziai_count)];
 }
-var_dump($bank_report);
 
 ?>
 <html>
 <head>
     <meta charset="UTF-8">
-    <style>
-        .income {
-            color: green;
-        }
-
-        .expense {
-            color: red;
-        }
-    </style>
 </head>
 <body>
-    <h1>Banko ataskaita</h1>
-    <h2><?php print "Balance: $balance eur"; ?></h2>
-    <h3><?php print "Įplaukos: $total_income eur"; ?></h3>
-    <h3><?php print "Išlaidos: $total_expense eur"; ?></h3>
-    <ul>
-        <?php foreach ($bank_report as $money_transfer): ?>
-            <li class="<?php print $money_transfer['css_class']; ?>">
-                <?php print $money_transfer['text']; ?>
-            </li>
+    <h1>Lietuvių egzaminas</h1>
+    <p>
+        <?php foreach ($rasinys as $zodis): ?>
+            <?php print $zodis; ?>
         <?php endforeach; ?>
-    </ul>
+    </p>
 </body>
 </html>
