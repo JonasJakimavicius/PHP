7b92bb00fef03eb189c4965ad897e052a24890ba
Jonas <jakimaviciusjonas@gmail.com>
2019-09-19 14:13:00 +0300

    Bank report (namu darbas)

--- index.php
+++ index.php
@@ -21,13 +21,23 @@ $bank_report =
         ],
     ];
 
+$balance = 0;
+$total_income = 0;
+$total_expense = 0;
+
 foreach ($bank_report as $money_transfer_id => $money_transfer) {
+    $balance += $money_transfer['amount'];
+
     if ($bank_report[$money_transfer_id]['amount'] > 0) {
         $bank_report[$money_transfer_id]['css_class'] = 'income';
+        $total_income += $money_transfer['amount'];
     } else {
         $bank_report[$money_transfer_id]['css_class'] = 'expense';
+        $total_expense += $money_transfer['amount'];
     }
+    $bank_report[$money_transfer_id]['text'] = "{$money_transfer['name']} : {$money_transfer['amount']}";
 }
+var_dump($bank_report);
 
 ?>
 <html>
@@ -45,10 +55,13 @@ foreach ($bank_report as $money_transfer_id => $money_transfer) {
 </head>
 <body>
     <h1>Banko ataskaita</h1>
+    <h2><?php print "Balance: $balance eur"; ?></h2>
+    <h3><?php print "Įplaukos: $total_income eur"; ?></h3>
+    <h3><?php print "Išlaidos: $total_expense eur"; ?></h3>
     <ul>
         <?php foreach ($bank_report as $money_transfer): ?>
             <li class="<?php print $money_transfer['css_class']; ?>">
-                <?php print "{$money_transfer['name']} : {$money_transfer['amount']} eur"; ?>
+                <?php print $money_transfer['text']; ?>
             </li>
         <?php endforeach; ?>
     </ul>
