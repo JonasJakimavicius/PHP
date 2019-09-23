2f03dda45b4508e110840f0e579e73f090c425de
Jonas <jakimaviciusjonas@gmail.com>
2019-09-23 15:16:40 +0300

    Masyvo pervertimo kodas. Jei norim non stop imtis kas kazkelinta reiksme.

--- index.php
+++ index.php
@@ -1,156 +1,22 @@
 <?php
 
-$thermometer = [
-    ['class' => 'bg-green', 'text' => 'PX'],
-    ['class' => 'bg-yellow', 'text' => 'BL'],
-    ['class' => 'bg-orange', 'text' => 'NX'],
-    ['class' => 'bg-red', 'text' => 'PZ2A'],
-];
+$strings = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 
-$stories = [
-    [
-        'class' => 'text-green',
-        'stories' => [
-            'Atsikėliau',
-            'Prabudau',
-            'Išlipau iš lovos'
-        ],
-    ],
-    [
-        'class' => 'text-yellow',
-        'stories' => [
-            'Vėl pirmadienis',
-            'Apšalę mašinos langai',
-            'Nėra ką valgyt pusryčiams'
-        ],
-    ],
-    [
-        'class' => 'text-orange',
-        'stories' => [
-            'Pavėlavau į darbo meetą',
-            'Pamiršau cigaretes',
-            'Balandis apšiko švarką'
-        ],
-    ],
-    [
-        'class' => 'text-red',
-        'stories' => [
-            'Užtvindžiau kaimynus',
-            'Mane užtvindė kaimynai',
-            'Sumaišiau panaudotą katės kraiką su šokoladu'
-        ],
-    ],
-];
+$count_strings_array = count($strings) - 1;
+$random_string = $strings[rand(0, $count_strings_array)];
+$root = array_rand($strings);
 
-$target_state = rand(1, 4);
+$song = [];
 
-foreach ($thermometer as $state_idx => $state) {
-    if ($state_idx < $target_state) {
-        // Set Story
-        $story_idx = array_rand($stories[$state_idx]['stories']);
-        $stories[$state_idx]['chosen_story'] = $stories[$state_idx]['stories'][$story_idx];
-    } else {
-        $thermometer[$state_idx]['class'] = 'white';
-        unset($stories[$state_idx]);
-    }
+for ($i = 1; $i < 4; $i++) {
+    $every_th_numb=2;
+    $song[] = $strings[$root];
+    $root = $root + $every_th_numb;
 
-    if ($state_idx !== $target_state) {
-        $thermometer[$state_idx]['name'] = '';
+    if ($root > $count_strings_array ) {
+        $root=$root-$count_strings_array-1;
     }
 }
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Thermometer</title>
-    <style>
-        body {
-            background: black;
-        }
-
-        .container {
-            display: flex;
-            align-items: center;
-        }
-
-        .thermo-circle, .thermo-box {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            font-weight: bold;
-            color: white;
-            margin-left: 10px;
-        }
-
-        .thermo-circle {
-            width: 100px;
-            height: 100px;
-            background-color: green;
-            border-radius: 100%;
-        }
-
-        .thermo-box {
-            width: 100px;
-            height: 50px;
-            border: 2px solid grey;
-        }
-
-
-        .bg-green {
-            background-color: green;
-        }
-
-        .bg-yellow {
-            background-color: #f3f309;
-        }
-
-        .bg-red {
-            background-color: red;
-        }
-
-        .bg-orange {
-            background-color: orange;
-        }
-
-        .bg-white {
-            background-color: white;
-        }
-
-        .text-green {
-            color: green;
-        }
-
-        .text-yellow {
-            color: yellow;
-        }
-
-        .text-orange {
-            color:  orange;
-        }
-
-        .text-red {
-            color: red;
-        }
-    </style>
-</head>
-<body>
-    <div class="container">
-        <div class="thermo-circle">0</div>
-
-        <?php foreach ($thermometer as $state_idx => $state) : ?>
-            <div class="thermo-box <?php print $state['class']; ?>">
-                <?php print $state['text']; ?>
-            </div>
-        <?php endforeach; ?>
-    </div>
-    <ul>
-        <?php foreach ($stories as $story): ?>
-            <li class="<?php print $story['class']; ?>">
-                <?php print $story['chosen_story']; ?>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-</body>
-</html>
+var_dump($song);
+print $count_strings_array;
