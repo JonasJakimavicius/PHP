24f4a9232d7af989f9bce102da5d1cb24866129c
Jonas <jakimaviciusjonas@gmail.com>
2019-09-24 14:19:44 +0300

    Drinks masyvo retail kaina su html su reguliariom ir akcinem kainom -pabaigtas variantas

--- index.php
+++ index.php
@@ -30,8 +30,18 @@ $drinks =
 
 foreach ($drinks as $drink_id => $drink) {
     $drinks[$drink_id]['price_retail'] = round($drink['price_stock'] - ($drink['price_stock'] / 100 * $drink['discount']), 2);
-    $drinks[$drink_id]['price_retail_text']= '€' . $drinks[$drink_id]['price_retail'] ;
-    $drinks[$drink_id]['price_stock_text']='€' .  $drinks[$drink_id]['price_stock'];
+    $drinks[$drink_id]['price_retail_text'] = '€' . $drinks[$drink_id]['price_retail'];
+    $drinks[$drink_id]['price_stock_text'] = '€' . $drinks[$drink_id]['price_stock'];
+    $drinks[$drink_id]['in_stock'] = rand(0, 1);
+
+    if ($drinks[$drink_id]['in_stock']) {
+        $drinks[$drink_id]['stock_text'] = 'Yra sandėlyje';
+        $drinks[$drink_id]['stock_class'] = 'text_green';
+
+    } else {
+        $drinks[$drink_id]['stock_text'] = 'Nėra sandėlyje';
+        $drinks[$drink_id]['stock_class'] = 'text_red';
+    }
 
     if ($drinks[$drink_id]['price_retail'] < $drink['price_stock']) {
         $drinks[$drink_id]['price_class'] = 'price_bigger';
@@ -49,7 +59,7 @@ foreach ($drinks as $drink_id => $drink) {
     <style>
         img {
             height: 200px;
-            width: 100px;
+            width: 180px;
             background-position: center;
             background-size: cover;
             margin: auto;
@@ -57,11 +67,12 @@ foreach ($drinks as $drink_id => $drink) {
         }
 
         .container {
-            width: 120px;
-            height: 280px;
+            width: 180px;
+            height: 300px;
             border: solid black 1px;
             float: left;
             margin-right: 5px;
+            position: relative;
         }
 
         .price {
@@ -69,9 +80,8 @@ foreach ($drinks as $drink_id => $drink) {
             width: 50px;
             background-color: deeppink;
             color: white;
-            position: relative;
-            bottom: 220px;
-            left: 70px;
+            position: absolute;
+            top: 0;
         }
 
         .price_bigger {
@@ -79,15 +89,18 @@ foreach ($drinks as $drink_id => $drink) {
             width: 50px;
             background-color: red;
             color: white;
-            position: relative;
-            bottom: 220px;
-            left: 70px;
+            position: absolute;
+            top: 0;
+            right: 0;
+
         }
 
         .title {
-            height: 20px;
+            height: 50px;
             width: 80px;
             margin: auto;
+            text-align: center;
+
         }
 
         .price_without_discount {
@@ -95,13 +108,31 @@ foreach ($drinks as $drink_id => $drink) {
             width: 50px;
             background-color: grey;
             color: white;
-            position: relative;
-            bottom: 250px;
+            position: absolute;
+            top: 0;
 
         }
 
         .price_regular {
             display: none;
+        }
+
+        .text_red {
+            color: red;
+            width: 80px;
+            margin: auto;
+            margin-bottom: 30px;
+            text-align: center;
+
+        }
+
+        .text_green {
+            color: green;
+            width: 80px;
+            margin: auto;
+            margin-bottom: 30px;
+            text-align: center;
+
         }
     </style>
 </head>
@@ -113,6 +144,7 @@ foreach ($drinks as $drink_id => $drink) {
             <div class="title"><?php print $drink['name']; ?> </div>
             <div class="<?php print $drink['price_class']; ?>"><?php print $drink['price_retail_text']; ?></div>
             <div class="<?php print $drink['discount_price_class']; ?> "><?php print $drink['price_stock_text']; ?> </div>
+            <div class="<?php print $drink['stock_class']; ?> "><?php print $drink['stock_text']; ?> </div>
         </div>
     <?php endforeach; ?>
 </body>
