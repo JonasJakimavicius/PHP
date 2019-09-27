44499b65467fc5c7f6076131a8ac10f7c3182193
Jonas <jakimaviciusjonas@gmail.com>
2019-09-27 14:51:54 +0300

    REFERENCE Uzduotis su avim

--- index.php
+++ index.php
@@ -1,88 +1,12 @@
 <?php
 
-$zodynas =
-    [
-        'bala',
-        'traalal',
-        'mano' =>
-            [
-                'balalaika',
-                'tralialia',
-                'bama' =>
-                    [
-                        'dama',
-                        'rama',
-                        'ruta',
+$sheeps = ['bleee'];
 
-                    ]
-
-            ],
-
-    ];
-
-
-
-function array_flatten($array)
-{
-    if (!is_array($array)) {
-        return FALSE;
-    }
-    $result = [];
-    foreach ($array as $key => $value) {
-        if (is_array($value)) {
-            $result = array_merge($result, array_flatten($value));
-        } else {
-            $result[$key] = $value;
-        }
-    }
-
-    return $result;
-
+//for'as  su reference
+for ($i = 0; $i < 5; $i++) {
+    $sheeps[] =& $sheeps[$i];
 }
 
-$array_flatten = array_flatten($zodynas);
-var_dump($array_flatten);
 
-function print_all($array)
-{
-    $count = count($array) - 1;
-    for ($i = 0; $i <= $count; $i++) {
-        if ($i != $count) {
-            print $array[$i] . ',';
-        } else {
-            print $array[$i] . '.';
-        }
-    }
-}
-
-$print_all = print_all($array_flatten);
-
-
-
-/*
- * Funkcija, kuri isspausdina visus masyvo elementus nepriklausomai nuo masyvo gylio
- * */
-
-
-//function printAll($array)
-//{
-//    if (!is_array($array)) {
-//        print $array . ', ';
-//        return;
-//    }
-//
-//    foreach ($array as $v_id => $v) {
-//        printAll($v);
-//
-//    }
-//}
-
-//
-//var_dump( $array);
-
-
-//$print_all = printAll($zodynas);
-
-
-
-
+$sheeps[3]='trasas';
+var_dump($sheeps);
