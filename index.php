10215d0c14041d824a5acddce45806e86dba110e
Jonas <jakimaviciusjonas@gmail.com>
2019-09-30 11:09:11 +0300

    Formos. 3 užduotis. Forma, kurios mygtuko verte padideja vienu ji paspaudus ir atspausdinama tiek paveiksleliu, kiek parasyta ant mygtuko.

--- index.php
+++ index.php
@@ -13,10 +13,23 @@ if (isset($_POST['submit'])) {
 <html>
 <head>
     <meta charset="UTF-8">
+    <style>
+        .img {
+            background-image: url("https://assets.shop.loblaws.ca/products_jpeg/20175355001/en/20175355001_lrg_1_@1x.jpg");
+            height: 100px;
+            width: 100px;
+            background-size: cover;
+            background-position: center;
+            display: inline-block;
+        }
+    </style>
 </head>
 <body>
     <form method="post">
         <input type="submit" name="submit" value="<?php print $button_number; ?>">
     </form>
+    <?php for ($i = 0; $i < $button_number; $i++): ?>
+        <div class="<?php print 'img'; ?>"></div>
+    <?php endfor; ?>
 </body>
 </html>
