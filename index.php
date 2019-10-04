29583ceb2201a9e4b2ca212d2ce31e8738398e3e
Jonas <jakimaviciusjonas@gmail.com>
2019-10-04 09:13:39 +0300

    Formos. Automatinis formų generavimas su template. Su Sigito kodu, kuris istato filtra i inputus

--- index.php
+++ index.php
@@ -11,41 +11,118 @@ $form =
             ],
         'fields' =>
             [
-                'email' => [
-                    'type' => 'email',
-                    'placeholder' => 'Email',
-                    'label' => 'E-mail',
-                ],
-                'name' => [
-                    'type' => 'text',
-                    'placeholder' => 'Name',
-                    'label' => 'Vardas',
-                    'error' => 'Klaida!',
+                'email' =>
+                    [
+                        'attr' =>
+                            [
+                                'type' => 'email',
+                                'placeholder' => 'Email',
+                            ],
+                        'extra' =>
+                            [
+                                'label' => 'E-mail',
+                            ],
+                        'error' => 'Klaida, nera pasto!',
+                        'filter' => FILTER_SANITIZE_SPECIAL_CHARS,
+                    ],
+                'vardas' =>
+                    [
+                        'attr' =>
+                            [
+                                'type' => 'text',
+                                'placeholder' => 'Name',
+                            ],
+                        'extra' =>
+                            [
+                                'label' => 'Vardas',
 
-                ],
-                'surname' => [
-                    'type' => 'text',
-                    'placeholder' => 'Surname',
-//                    'label' => 'Pavardė',
+                            ],
+                        'error' => 'Klaida, nera vardo!',
+                        'filter' => FILTER_SANITIZE_SPECIAL_CHARS,
+                    ],
+                'name' =>
+                    [
+                        'attr' =>
+                            [
+                                'type' => 'select',
+                                'placeholder' => 'Name',
 
-                ],
+                            ],
+                        'extra' =>
+                            [
+                                'options' =>
+                                    [
+                                        'tadas' => 'Tadas',
+                                        'aurimas' => 'Aurimas',
+                                        'dainius' => 'Dainius',
+
+                                    ],
+                                'label' => 'vardas',
+                                'error' => 'Klaida, nera name!',
+                                'filter' => FILTER_SANITIZE_SPECIAL_CHARS,
+                            ],
+                    ],
+                'surname' =>
+                    [
+                        'attr' =>
+                            [
+                                'type' => 'text',
+                                'placeholder' => 'Surname',
+
+                            ],
+                        'extra' =>
+                            [
+                                'label' => 'Pavardė',
+                            ],
+                        'error' => 'Klaida, nera pavardes!',
+                        'filter' => FILTER_SANITIZE_SPECIAL_CHARS,
+                    ],
             ],
         'buttons' =>
             [
-                [
-                    'type' => 'submit',
-                ],
+                'button' =>
+                    [
+
+                        'type' => 'submit',
 
 
+                    ],
             ],
+        'message' => 'Message!',
+
     ];
 
 require('functions/functions.php');
+// sigito kodas
+
+function get_filtered_input($array)
+{
+    $filter_parameters = [];
+
+    foreach ($array['fields'] as $idx => $inner_array) { // jei true, su tuo masyvu, daryk tai:
+        $filter_parameters[$idx] = $inner_array['filter'] ?? FILTER_SANITIZE_SPECIAL_CHARS; // ideda filtra (konstantą) kokia nurodyta masyve.
+    };
+    return filter_input_array(INPUT_POST, $filter_parameters); // sitoj silutej vyskta sanitation pagal default f-ja
+}
+
+$filtered_input = get_filtered_input($form);
+var_dump($filtered_input);
+
+foreach ($form['fields'] as $inner_array_idx => &$inner_array) { // cia padaro, jog po submit paspaudimo, i inputa grazina isvalyta rezultata, aks buvo ivesta.
+    $inner_array['attr']['value'] = $filtered_input[$inner_array_idx];
+
+    if ($inner_array['attr']['value'] === '') { //jeigu $form'oje nera 'error', tai iraso toki elementa su nurodyta value.
+        $inner_array['error'] = 'klaida';
+    }
+};
+
+// sigito kodo pabaiga
 
 ?>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
+    <title>Formos generavimas</title>
 </head>
 <body>
     <?php require('templates/form.tpl.php'); ?>
--- templates/form.tpl.php
+++ templates/form.tpl.php
@@ -1,35 +1,56 @@
-<!--prasideda  form tago atributu generavimas-->
 <form <?php print html_attr(($form['attr']) + ['method' => 'post']); ?>>
-    <!--pbaigiasi form tago atributu generavimas-->
 
-    <!--    prasideda imputu atributu generavimas-->
-    <?php foreach ($form['fields'] ?? [] as $field_id => $field): ?>
+    <?php if (isset ($form['fields'])): ?>
+        <div class="input-container">
 
-        <!--prasideda labeliu spausdinimas, jei jie yra nustatyti mmasyve-->
-        <?php if (isset($field['label'])) : ?>
-            <label>
-            <?php print $field['label']; ?>
-        <?php endif; ?>
-        <!--baigiasi labeliu spausdinimas, jei jie yra nustatyti mmasyve-->
 
-        <!--išspausdinam inputus su jiems nustatytais atributais-->
-        <input <?php print html_attr(['type' => $field['type'], 'name' => $field_id, 'placeholder' => $field['placeholder']]); ?>>
-        <!--    išspausdinam errora, jei jis yra nurodytas masyve-->
-        <div> <?php print $field['error'] ?? ''; ?> </div>
+            <?php foreach ($form['fields'] ?? [] as $field_id => $field): ?>
 
-        <!--    Patikrinam ar label yra setintas-->
-        <?php if (isset($field['label'])) : ?>
-            </label>
-        <?php endif; ?>
-        <!--    Baigiam patikrina ar label yra setintas-->
 
-    <?php endforeach; ?>
-    <!--    Baigiasi imputu atributu generavimas-->
+                <?php if (isset($field['extra']['label'])) : ?>
+                    <label>
+                    <?php print $field['extra']['label']; ?>
+                <?php endif; ?>
 
-    <!--    Prasideda mygtukų atributu generavimas-->
-    <?php foreach ($form['buttons'] ?? [] as $button_id => $button): ?>
-        <button <?php print html_attr($button); ?> ></button>
-    <?php endforeach; ?>
-    <!--    Baigiasi mygtukų atributu generavimas-->
+                <?php if ($field['attr']['type'] === 'select'): ?>
+                    <select <?php print html_attr(['name' => $field_id,]); ?>>
+                        <?php foreach ($field['extra']['options'] as $select_value_id => $select_value): ?>
+                            <option value="<?php print $select_value_id; ?>"><?php print $select_value; ?> </option>
+                        <?php endforeach; ?>
+                    </select>
+
+                <?php else: ?>
+
+                    <input <?php print html_attr((['name' => $field_id,]) + $field['attr'] ?? []); ?>>
+                <?php endif; ?>
+
+                <?php if (isset($field['error'])): ?>
+                    <div> <?php print $field['error']; ?></div>
+                <?php endif; ?>
+
+
+                <?php if (isset($field['label'])) : ?>
+                    </label>
+                <?php endif; ?>
+
+
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
+
+
+
+
+    <?php if (isset ($form['buttons'])): ?>
+        <div class="buttons-container">
+
+            <?php foreach ($form['buttons'] ?? [] as $button_id => $button): ?>
+                <button <?php print html_attr($button); ?> ></button>
+            <?php endforeach; ?>
+
+        </div>
+    <?php endif; ?>
+
+    <?php print $form['message'] ?? ''; ?>
 </form>
 
