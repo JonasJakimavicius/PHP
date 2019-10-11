44cb71cd38c839f7c3f20615368fa42d24e44127
Jonas <jakimaviciusjonas@gmail.com>
2019-10-11 09:49:09 +0300

    Cookie Sugeneruoja useri ir paraso, kiek kartu jis buvo uzejes

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require('functions/form/core.php');
 require('functions/html/generators.php');
-require('functions/file/json_transform.php');
+require('functions/file.php');
 
 $form = [
     'attr' => [
@@ -57,21 +57,48 @@ function update_file($filtered_input)
     $array = file_to_array('data/masyvas.txt');
     $array[] = $filtered_input;
     array_to_file('data/masyvas.txt', $array);
+
 }
+
 
 function form_success($filtered_input)
 {
     update_file($filtered_input);
-    setcookie('kukis', 'pilde', time() + 5, '/');
+    setcookie('kukis', json_encode($filtered_input), time() + 20, '/');
 }
 
-function form_fail()
+function form_fail($filtered_input, &$form)
 {
+    $fail = setcookie('kuksis', json_encode($filtered_input), time() + 40, '/');
     var_dump('Retard alert');
 }
 
-$users_info = is_array(file_to_array('data/masyvas.txt')) ? file_to_array('data/masyvas.txt') : [];
+if (isset($_COOKIE['kuksis'])) {
+    $array_from_cookie = json_decode($_COOKIE['kuksis'], true);
+    $form['fields']['nickname']['attr']['value'] = $array_from_cookie['nickname'];
+    $form['fields']['password']['attr']['value'] = $array_from_cookie['password'];
+    $array_from_cookie = [];
 
+}
+
+$user_id = rand(1, 999999);
+
+
+if (!isset($_COOKIE['kukutis'])) {
+    $user_data = [
+        'user_id' => $user_id,
+        'time' => 1,
+    ];
+    $kukutis = setcookie('kukutis', json_encode($user_data), time() + 30, '/');
+}
+
+if (isset($_COOKIE['kukutis'])) {
+    $array_from_cookie = json_decode($_COOKIE['kukutis'], true);
+    $array_from_cookie['time'] += 1;
+    setcookie('kukutis', json_encode($array_from_cookie), time() + 30, '/');
+}
+
+$users_info = is_array(file_to_array('data/masyvas.txt')) ? file_to_array('data/masyvas.txt') : [];
 
 ?>
 <html lang="en">
@@ -90,6 +117,10 @@ $users_info = is_array(file_to_array('data/masyvas.txt')) ? file_to_array('data/
             </tr>
         <?php endforeach; ?>
     </table>
+<?php endif; ?>
+<?php if (isset($_COOKIE['kukutis'])): ?>
+    <h1><?php print $array_from_cookie['user_id']; ?> </h1>
+    <h2><?php print $array_from_cookie['time']; ?></h2>
 <?php endif; ?>
 
 </body>
--- uzduotys.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-var_dump($_COOKIE);
-
-setcookie('mycookie', 'cookiedata', time()+3600,'/');
